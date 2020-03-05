d91bcb5ae6abe445c4cc0d7991d889aa4d9bba83
dependable-framework-2948 <bryancapitulo@gmail.com>
2020-03-05 20:27:31 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,9 +1,8 @@
-x=10
-
-def countdown (10)
-  while x
-    puts "#{x} SECONDS!"
-    x -= 1
-    "HAPPY NEW YEAR!"
-  end
+def countdown
+ x = 10
+ while x > 0
+ puts "#{x} SECOND(S)!"
+ x -= 1
+ end
+ puts "HAPPY NEW YEAR!"
 end
