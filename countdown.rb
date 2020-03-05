95cc3ea21ae4174c6d8b73f3ecb1cad470a13ebc
dependable-framework-2948 <bryancapitulo@gmail.com>
2020-03-05 22:29:43 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,12 +1,12 @@
 def countdown(number)
   while number > 0
     puts "#{number} SECOND(S)!"
-    sleep(0.5)
+    sleep(1)
     number -= 1
   end
   "HAPPY NEW YEAR!"
 end
 
 def countdown_with_sleep
-  sleep (0.5)
+  sleep (1)
 end
